744f81a6048bc0f7de114075304c1b6923d79c5c
Ryan <corkery.ryan@gmail.com>
2019-08-16 15:54:59 +0800

    nav sticky

--- js/main.js
+++ js/main.js
@@ -10,3 +10,25 @@ function hamburgerOpen(){
 function hamburgerClose(){
 	document.querySelector(".navRight").style.display="none";
 }
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+	if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+		document.querySelector(".navUpper").style.height = "40px";
+	}
+	else{
+		document.querySelector(".navUpper").style.height = "80px";
+	}
+
+	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+		document.querySelector("nav img").src = "img/logo.png";
+		document.querySelector("nav img").style.height = "30px";
+	}
+	else{
+		document.querySelector("nav img").src = "img/logo-text.png";
+		document.querySelector("nav img").style.height = "70px";
+	}
+}
