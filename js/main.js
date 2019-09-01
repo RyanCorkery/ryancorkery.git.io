71f1a7b4e8571aefa3e3a14c326141dd18fac572
Ryan <corkery.ryan@gmail.com>
2019-09-01 22:22:51 +0800

    tablet

--- js/main.js
+++ js/main.js
@@ -14,27 +14,35 @@ function hamburgerClose(){
 
 
 
+
+
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction(){
-	if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-		document.querySelector(".navUpper").style.height = "60px";
-	}
-	else{
-		document.querySelector(".navUpper").style.height = "80px";
-	}
 
-	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-		document.querySelector("nav img").src = "img/logo.png";
-		document.querySelector("nav img").style.height = "30px";
-		document.querySelector(".hamburgerClose").style.height = "50px"
-	}
-	else{
-		document.querySelector("nav img").src = "img/logo-text.png";
-		document.querySelector("nav img").style.height = "70px";
-		document.querySelector(".hamburgerClose").style.height = "70px"
+	var mq = window.matchMedia( "(max-width: 480px)" );
+	if (mq.matches) {
+		if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+		document.querySelector(".navUpper").style.height = "60px";
+		}
+		else{
+			document.querySelector(".navUpper").style.height = "80px";
+		}
+
+		if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+			document.querySelector("nav img").src = "img/logo.png";
+			document.querySelector("nav img").style.height = "30px";
+			document.querySelector(".hamburgerClose").style.height = "50px"
+		}
+		else{
+			document.querySelector("nav img").src = "img/logo-text.png";
+			document.querySelector("nav img").style.height = "70px";
+			document.querySelector(".hamburgerClose").style.height = "70px"
+		}
 	}
 }
+
 
 
 
